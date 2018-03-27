f0a41a4f359e507e1f234b3dac171c3e4ad7cf45
gabrielmuller <gabrielmullerw@gmail.com>
2018-03-26 21:43:57 -0300

    norm mais otimizado

--- window.cpp
+++ window.cpp
@@ -1,4 +1,5 @@
 #include "window.h"
+#include "math.h"
 
 Vector2 Window::position(0, 0);
 Vector2 Window::viewport(400, 400);
@@ -28,22 +29,37 @@ Vector2 Window::world_to_screen(Vector2 coords) {
 // converte uma coordenada do espaço no mundo para
 // coordenada normalizada
 Vector2 Window::world_to_norm (Vector2 coords) {
-    Vector2 amount (2 / size.x(), 2 / size.y());
-    Transformation* rot = Transformation::rotation(angle, position);
-    Transformation* scale = Transformation::scaling(amount, position);
-    Transformation* move = Transformation::translation(Vector2(-position.x(), -position.y()));
-    //ta horrível
-    coords = *(*(*((Transformation) coords * *rot) * *scale) * *move);
-    delete rot;
-    delete scale;
+    Transformation t (3, 3);
+    float s = sin(angle);
+    float c = cos(angle);
+    float a = 2 / size.x();
+    float b = 2 / size.y();
+    float x = position.x();
+    float y = position.y();
+
+    t.matrix = {
+        {a*c, -b*s, 0},
+        {a*s,  b*c, 0},
+        {a*(-c*x - s*y), b*(s*x - c*y), 1}
+    };
+
+    coords = *((Transformation) coords * t);
     return coords;
 }
 
 Vector2 Window::norm_to_vp (Vector2 coords) {
     Vector2 amount (viewport.x() / 2, -viewport.y() / 2);
+    float a = viewport.x() / 2;
+    float b = -viewport.y() / 2;
     Transformation* move = Transformation::translation(Vector2(1, -1));
     Transformation* scale = Transformation::scaling(amount, Vector2(0, 0));
-    return *(*((Transformation) coords * *move) * *scale);
-}
+    Transformation t (3, 3);
+    t.matrix = {
+        {a,  0, 0},
+        {0,  b, 0},
+        {a, -b, 1}
+    };
 
-    
+    coords = *((Transformation) coords * t);
+    return coords;
+}

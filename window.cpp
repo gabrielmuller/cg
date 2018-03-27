12871823d1d29518756e4563d5d1478179f945a0
gabrielmuller <gabrielmullerw@gmail.com>
2018-03-27 19:02:39 -0300

    tirar comentários

--- window.cpp
+++ window.cpp
@@ -10,21 +10,8 @@ Vector2 Window::size(10, 10);
 
 // converte uma coordenada do espaço no mundo para tela
 Vector2 Window::world_to_screen(Vector2 coords) {
-    /*Transformation* rot = Transformation::rotation(angle, Window::position);
-    coords = *((Transformation) coords * *rot);
-
-    Vector2 wmin = Window::position - (Window::size / 2);
-    Vector2 output(coords - wmin);
-    output = Vector2(output.x() / Window::size.x(), output.y() / Window::size.y());
-    output.matrix[0][1] = 1 - output.y();
-    output = Vector2(output.x() * Window::viewport.x(), output.y() * Window::viewport.y());
-
-    return output;*/
     return norm_to_vp(world_to_norm(coords));
 }
-
-
-// TODO: otimizar
 
 // converte uma coordenada do espaço no mundo para
 // coordenada normalizada

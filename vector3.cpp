beaad7b08bedaf0b7f03dd65f0a138a720bb1503
gabrielmuller <gabrielmullerw@gmail.com>
2018-04-20 19:12:57 -0300

    Início de Vector3

---
+++ vector3.cpp
@@ -0,0 +1,38 @@
+#include "vector3.h"
+
+Vector3::Vector3 () : Vector3::Vector3(0, 0, 0) {}
+  
+Vector3::Vector3 (float x, float y, float z) : Transformation(1, 4) {
+    // teste
+    matrix = {{x, y, z, 1}};
+}
+
+Vector3::Vector3 (const Transformation& other) : Transformation(1, 4) {
+    matrix[0] = other.matrix[0];
+}
+
+float Vector3::x() const {
+    return matrix[0][0];
+}
+
+float Vector3::y() const {
+    return matrix[0][1];
+}
+
+float Vector3::z() const {
+    return matrix[0][2];
+}
+
+Vector3::operator std::string () const {
+    return "(" + std::to_string(x()) + ", " 
+        + std::to_string(y())
+        + std::to_string(z()) + ")";
+}
+
+bool Vector3::operator== (Vector3 other) const {
+    return x() == other.x()  
+        && y() == other.y()
+        && y() == other.z();
+}
+
+    
---
+++ vector3.h
@@ -0,0 +1,24 @@
+#ifndef VECTOR3_H
+#define VECTOR3_H
+
+#include <string>
+
+#include "transformation.h"
+
+struct Vector3 : Transformation {
+    float x() const;
+    float y() const;
+    float z() const;
+
+    Vector3();
+    Vector3 (float x, float y, float z); 
+    Vector3 (const Transformation& other); 
+
+    operator std::string () const;
+    bool operator== (Vector3 other) const;
+
+    static Vector3 lerp (const Vector3& a, const Vector3& b, float p);
+
+};
+
+#endif
